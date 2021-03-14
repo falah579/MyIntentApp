6ff715b90b90b4c8cefdeedf10b9f6e6a2a7a858
Falah Muhammad Furqon <falahmuhammadfurqon@Razka.local>
2021-03-14 21:39:36 +0700

    Initial commit

---
+++ app/src/main/java/com/idn/falah/myintentapp/MoveActivity.kt
@@ -0,0 +1,11 @@
+package com.idn.falah.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
